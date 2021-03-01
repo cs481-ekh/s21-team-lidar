a6f022c74c6dfb39d9dc79993bc17d2a97ecb56e
WesleyYWong <wesleywong@u.boisestate.edu>
2021-02-28 17:09:45 -0700

    Make organized point cloud #6

---
+++ src/MatLab/organizedpointcloud.m
@@ -0,0 +1,30 @@
+x = tst();
+
+width = 1024*6.5;
+height = 64*6.5;
+
+points = zeros(height,width,3);
+intensityMap = zeros(height,width);
+newMatrix = cell(1,size(x,1));
+for cloudnum = 1:1.0:size(x,1)
+    y = x{cloudnum,1};
+    XResolution = abs(y.XLimits(2) - y.XLimits(1)) / width;
+    YResolution = abs(y.YLimits(2) - y.YLimits(1)) / height;
+    for v = 1:1.0:size(y.Location,1)
+        sample = y.Location(v,:);
+        N = round(abs(sample(1) - y.XLimits(1)) / XResolution);
+        if (N == 0)
+            N = 1;
+        end
+        M = round(abs(sample(2) - y.YLimits(1)) / YResolution);
+        if (M == 0)
+            M = 1;
+        end
+        points(M,N,1) = sample(1);
+        points(M,N,2) = sample(2);
+        points(M,N,3) = sample(3);
+        intensityMap(M,N) = y.Intensity(v);
+    end 
+    ptCloud = pointCloud(points, 'Intensity', intensityMap);
+    newMatrix{1,cloudnum} = ptCloud;
+end
